a7076373cb9c3cbfdd0d3d68baafffdad12c9987
4vyrez <160358722+4vyrez@users.noreply.github.com>
2026-03-24 16:19:05 +0100

    refactor: update error handling mechanisms and improve logging for server operations, ensuring consistency in build ID references across manifests and error pages

---
+++ apps/marketing/.next/server/middleware-build-manifest 2.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/a0b0651250389690.js",
+    "static/chunks/aee6c7720838f8a2.js",
+    "static/chunks/ef0dc6461e9250d8.js",
+    "static/chunks/turbopack-a61125d9706c61b2.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
---
+++ apps/marketing/src/components/public/sections/index 2.ts
@@ -0,0 +1,8 @@
+export * from './hero';
+export * from './about';
+export * from './history';
+export * from './community';
+
+export * from './university';
+export * from './house';
+export * from './contact';
